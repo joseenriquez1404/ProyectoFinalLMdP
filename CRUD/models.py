09c0ccde149e3d5002ea0f5db0b47baddb23365c
diego <diegoadrianm>
2024-10-21 12:47:29 -0600

    creacion de modelos

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Equipo(models.Model):
+    nombre_equipo = models.CharField(max_length=100)
+    pais_origen = models.CharField(max_length=100)
+    estado_origen = models.CharField(max_length=100)
+
+
+class Jugador(models.Model):
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    nombre_jugador = models.CharField(max_length=200)
+    apellido_jugador = models.CharField(max_length=200)
+    numero_playera = models.IntegerField()
+    fecha_nacimiento = models.DateField("fecha nacimieto")
+    posicion_campo = models.CharField(max_length=100)
