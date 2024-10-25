4b96c1f4cb6dff59a22231a22e4a116c8e530621
joseenriquez1404 <manuel.enriquezr@uanl.edu.mx>
2024-10-25 12:35:11 -0600

    Fix routes

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("update/", views.updateMenu, name="updateMenu"),
-    path("update/show_teams/", views.showTeam, name="showTeam"),
-    path("update/show_teams/show_players/", views.showTeamPlayers, name="showTeamPlayers"),
-    path("update/show_teams/show_players/<int:equipo_id>/", views.teamPlayers, name="teamPlayers"),
-    path("update/show_teams/<int:jugador_id>/", views.editPlayer, name="editPlayer"),
-    path("update/show_teams/<int:equipo_id>/", views.editTeam, name="editTeam"),
+    path("update/team/show_teams/", views.showTeam, name="showTeam"),
+    path("update/player/show_teams/show_players/", views.showTeamPlayers, name="showTeamPlayers"),
+    path("update/player/show_teams/show_players/<int:equipo_id>/", views.teamPlayers, name="teamPlayers"),
+    path("update/player/show_teams/show_players/edit_player/<int:jugador_id>/", views.editPlayer, name="editPlayer"),
+    path("update/team/show_teams/<int:equipo_id>/", views.editTeam, name="editTeam"),
 ]
